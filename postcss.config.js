928a609eb2bd371c5bcdf8fcb8285dd137b3d669
Dilolabs <adn@dilolabs.fr>
2020-03-28 00:40:41 +0100

    Add PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./app/views/**/*.html.erb'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
     require('postcss-import'),
@@ -9,6 +17,7 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
+    purgecss
   ]
 }
