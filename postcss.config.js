7a6deb0f36640c049e9bb415812e3a66aa2e5916
Dilolabs <adn@dilolabs.fr>
2020-03-27 23:32:37 +0100

    Finish Tailwind CSS first configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.js'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
