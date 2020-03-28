2b0d0804b61b84b68b99b9bd791ce38ae30fb2eb
Cyril Blaecke <cyril@dilolabs.fr>
2020-03-28 16:41:08 +0100

    Fix spelling mistake

--- db/migrate/20200328140009_create_sessions.rb
+++ db/migrate/20200328140009_create_sessions.rb
@@ -2,7 +2,7 @@ class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions, id: :uuid do |t|
       t.references :classroom, type: :uuid, null: false, foreign_key: true
-      t.datetime :scheduled_dt
+      t.datetime :scheduled_at
       t.string :live_url
       t.references :user, type: :uuid, null: false, foreign_key: true
 
