b44fe07b512d68f1323dd8200f82dd587f876504
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-28 15:05:56 +0100

    create Resource model & table

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,4 @@
 class Classroom < ApplicationRecord
     has_many :sessions
+    has_many :resources
 end
---
+++ db/migrate/20200328140446_create_resources.rb
@@ -0,0 +1,12 @@
+class CreateResources < ActiveRecord::Migration[6.0]
+  def change
+    create_table :resources, id: :uuid do |t|
+      t.references :classroom, null: false, foreign_key: true
+      t.string :url
+      t.string :img_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
