d28be1a2b0e8216a86ae428af0c1471efc56e1e7
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-28 15:12:12 +0100

    fix uuid handling in migration

--- db/migrate/20200328140009_create_sessions.rb
+++ db/migrate/20200328140009_create_sessions.rb
@@ -1,10 +1,10 @@
 class CreateSessions < ActiveRecord::Migration[6.0]
   def change
     create_table :sessions, id: :uuid do |t|
-      t.references :classroom, null: false, foreign_key: true
+      t.references :classroom, type: :uuid, null: false, foreign_key: true
       t.datetime :scheduled_dt
       t.string :live_url
-      t.references :user, null: false, foreign_key: true
+      t.references :user, type: :uuid, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200328140446_create_resources.rb
+++ db/migrate/20200328140446_create_resources.rb
@@ -1,7 +1,7 @@
 class CreateResources < ActiveRecord::Migration[6.0]
   def change
     create_table :resources, id: :uuid do |t|
-      t.references :classroom, null: false, foreign_key: true
+      t.references :classroom, type: :uuid, null: false, foreign_key: true
       t.string :url
       t.string :img_url
       t.string :description
