35858ef3cabc3440c3d154b6b3c6da877aeb7aef
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-28 15:41:39 +0100

    update seed with Session instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# rails db:seed:replant
 
-Classroom.create([
-  { name: 'Maternelle', position: 1, color: 'orange', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-maternelle' },
-  { name: 'CP', position: 2, color: 'green', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-cp' },
-  { name: 'CE1', position: 3, color: 'red', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-ce1' },
-  { name: 'CE2', position: 4, color: 'blue', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-ce2' },
-  { name: 'CM1', position: 5, color: 'purple', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-cm1' },
-  { name: 'CM2', position: 6, color: 'yellow', videoconference_url: 'https://dilotalk.cloud/laclassealamaison-org-cm2' },
+puts "Building Classroom instances..."
+
+Classroom.create!([
+  { name: 'Maternelle', position: 1, color: 'orange' },
+  { name: 'CP', position: 2, color: 'green' },
+  { name: 'CE1', position: 3, color: 'red' },
+  { name: 'CE2', position: 4, color: 'blue' },
+  { name: 'CM1', position: 5, color: 'purple' },
+  { name: 'CM2', position: 6, color: 'yellow' },
 ])
+puts "...ok"
+
+
+puts "Building Session instances..."
+
+available_times = [
+  DateTime.new(2020, 4, 1, 9), # 9h le 4 avril 2020
+  DateTime.new(2020, 4, 1, 10),
+  DateTime.new(2020, 4, 1, 11),
+  DateTime.new(2020, 4, 1, 14),
+  DateTime.new(2020, 4, 1, 15),
+  DateTime.new(2020, 4, 1, 16),
+  DateTime.new(2020, 4, 2, 11),
+  DateTime.new(2020, 4, 3, 14),
+  DateTime.new(2020, 4, 4, 15),
+  DateTime.new(2020, 4, 5, 6)
+]
+
+Classroom.all.each do |classroom|
+  available_times.each do |available_time|
+    Session.create!({
+      classroom: classroom,
+      scheduled_dt: available_time,
+      live_url: nil,
+      user: nil
+    })
+  end
+end
+puts "...ok"
+
+puts "Success!"
