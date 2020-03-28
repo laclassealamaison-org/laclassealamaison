680ca3b6752bddd7ef99cd6b8a1064753dd6d099
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-28 23:03:06 +0100

    add name to Resource model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_194147) do
+ActiveRecord::Schema.define(version: 2020_03_28_215429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_194147) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["classroom_id"], name: "index_resources_on_classroom_id"
   end
 
