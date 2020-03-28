4bd68ec510ced90494d94740a46fb50a09ab9755
Dilolabs <adn@dilolabs.fr>
2020-03-28 17:38:09 +0100

    Add annotations

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: classrooms
+#
+#  id         :uuid             not null, primary key
+#  color      :string
+#  name       :string
+#  position   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Classroom < ApplicationRecord
     has_many :sessions
     has_many :resources
--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id           :uuid             not null, primary key
+#  live_url     :string
+#  scheduled_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  classroom_id :uuid             not null
+#  user_id      :uuid
+#
+# Indexes
+#
+#  index_sessions_on_classroom_id  (classroom_id)
+#  index_sessions_on_user_id       (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (classroom_id => classrooms.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
