278854e7e40a2eee616a9bb4b7b6664597a52895
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-03-29 17:05:15 +0200

    added unstyled list of upcoming sessions

--- app/models/classroom_animation.rb
+++ app/models/classroom_animation.rb
@@ -26,7 +26,8 @@ class ClassroomAnimation < ApplicationRecord
   belongs_to :classroom
   belongs_to :user
 
-  scope :live,            -> { where("starts_at < ? AND starts_at + interval '1 hour' > ?", Time.now, Time.now)}
+  scope :live,            -> { where("starts_at < current_timestamp AND starts_at + interval '1 hour' > current_timestamp") }
+  scope :upcoming,         -> { where("starts_at > current_timestamp") }
   # scope :open
   # scope :live_and_open
 end
