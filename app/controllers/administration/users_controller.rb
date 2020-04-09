a453c087f277528295bdfff63248ed46079aeef9
Gauthier Monserand <gauthier@squadracer.com>
2020-04-09 15:35:50 +0200

    Allow developer to impersonate user to switch profile quickly

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -3,4 +3,10 @@ class Administration::UsersController < ApplicationController
     @users = User.simple_users.order(created_at: :desc)
     authorize [:administration, :user], :index?
   end
+  
+  def impersonate
+    return unless Rails.env.development?
+    impersonate_user(User.find(params[:user_id]))
+    redirect_to root_path
+  end
 end
