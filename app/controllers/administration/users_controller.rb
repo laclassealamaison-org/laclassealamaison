63117fddb3fe314b93549dc3e3aed62f895fe4aa
Gauthier Monserand <gauthier@squadracer.com>
2020-04-14 16:50:58 +0200

    fix helper call

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -8,6 +8,6 @@ class Administration::UsersController < ApplicationController
     return unless Rails.env.development?
     user = User.find(params[:user_id])
     impersonate_user(user)
-    redirect_to home_path_for(user)
+    redirect_to helpers.home_path_for(user)
   end
 end
