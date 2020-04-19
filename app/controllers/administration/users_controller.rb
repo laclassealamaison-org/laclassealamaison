715a866fcfd7cf4a30ae52316e6bcc73f229cf22
Gauthier Monserand <gauthier@squadracer.com>
2020-04-19 20:58:09 +0200

    fix impersonate bug

--- app/controllers/administration/users_controller.rb
+++ app/controllers/administration/users_controller.rb
@@ -1,16 +1,17 @@
 class Administration::UsersController < ApplicationController
   include AdministrationUserConcern
 
-  private
-
-  def user_role
-    :user
-  end
-
   def impersonate
     return unless Rails.env.development?
     user = User.find(params[:user_id])
     impersonate_user(user)
     redirect_to helpers.home_path_for(user)
   end
+
+  private
+
+  def user_role
+    :user
+  end
+
 end
