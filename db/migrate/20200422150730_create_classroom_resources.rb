f2c7be4f5050d76aade3ddd2115c245d6317cfe9
Zya04 <zya.nassurally@supinternet.fr>
2020-05-14 15:27:24 +0200

    fixed conflit

--- db/migrate/20200422150730_create_classroom_resources.rb
+++ db/migrate/20200422150730_create_classroom_resources.rb
@@ -2,7 +2,6 @@ class CreateClassroomResources < ActiveRecord::Migration[6.0]
   def change
     create_table :classroom_resources, id: :uuid do |t|
       t.references :resource, null: false, foreign_key: true, type: :uuid
-<<<<<<< HEAD
       t.references :classroom, null: false, foreign_key: true, type: :uuid
       t.timestamps
     end
@@ -13,35 +12,6 @@ class CreateClassroomResources < ActiveRecord::Migration[6.0]
       resources[1..-1].map(&:destroy)
     end
 
-=======
-
-      t.timestamps
-    end
-  end
-
-  j = 0
-
-  all_resources = Resource.all.group_by(&:url)
-
-  # array of int
-  total_count_resources = all_resources.map do |url, resource| resource.count end
-
-  while j < total_count_resources.length
-    total_count_resources.each do |nb|
-
-      # out of idea
-
-      if nb > 1
-        all_resources.map do |url, resource|
-          if i > 1
-            # destroy resource
-          end
-        end
-      end
-    end
-
-    j += 1
->>>>>>> 7bb90d030ddf28878b1f540c8542bf48628cd0a8
   end
 
 end
--- spec/factories/resource_factory.rb
+++ spec/factories/resource_factory.rb
@@ -3,9 +3,5 @@ FactoryBot.define do
     name { FFaker::Lorem.sentence }
     description { FFaker::Lorem.sentence }
     url { FFaker::Youtube.url}
-<<<<<<< HEAD
-=======
-    classroom
->>>>>>> 7bb90d030ddf28878b1f540c8542bf48628cd0a8
   end
 end
