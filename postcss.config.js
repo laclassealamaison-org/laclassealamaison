bc72e58907bd018b73fc710a5c94f4eebbc54367
Gauthier Monserand <simkim@simkim.net>
2020-05-14 12:23:46 +0200

    Fix css wrong stripping

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./app/views/**/*.html.erb'],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml'
+  ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
